1508a00783e88f6db705fa4bd969ef91a30b8a48
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-07 15:47:06 -0300

    ACrescentando o inicio de estudo typescript em solid

--- src/srp/shopping-cart.ts
+++ src/srp/shopping-cart.ts
@@ -1,12 +1,14 @@
-type Item = {
+type ItemType = {
   name: string;
   price: number;
 };
+type OrderStatusType = 'open' | 'closed';
 
 export class ShoppingCart {
-  private readonly _items: Item[] = [];
+  private readonly _items: ItemType[] = [];
+  private _orderStatus: OrderStatusType = 'open';
 
-  AddItems(product: Item): void {
+  AddItems(product: ItemType): void {
     this._items.push(product);
   }
 
@@ -14,8 +16,46 @@ export class ShoppingCart {
     this._items.splice(index, 1);
   }
 
-  get items(): Readonly<Item[]> {
+  get items(): Readonly<ItemType[]> {
     return this._items;
+  }
+
+  get orderStatus(): OrderStatusType {
+    return this._orderStatus;
+  }
+
+  total(): number {
+    return +this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2);
+  }
+
+  checkOut(): void {
+    if (this.isEmpty()) {
+      console.log('Seu carrinho esta vazio');
+      return;
+    }
+    this.orderStatus = 'closed';
+    this.sendMessage(`Seu pedido com os produtos com total de ${this.total()} foi recebido`);
+    this.saveOrder();
+    this.clear();
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+
+  sendMessage(msg: string): void {
+    console.log('Mensagem', msg);
+  }
+
+  saveOrder(): void {
+    console.log('Pedido salvo');
+  }
+
+  clear(): void {
+    console.log('Vazio');
+    this._items.length === 0;
   }
 }
 
@@ -32,5 +72,8 @@ shoppingCart.AddItems({
   name: 'alface',
   price: 8.0,
 });
+shoppingCart.clear();
 
 console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+shoppingCart.checkOut();
