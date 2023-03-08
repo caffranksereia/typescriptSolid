a24b14bb35e9ce9a2b8f42f868f75642fda85548
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-08 16:10:46 -0300

    somente main

--- src/srp/main.ts
+++ src/srp/main.ts
@@ -1,20 +1,20 @@
+import { Messaging } from "./interfaces/message-interface";
+import { Persisting } from "./interfaces/persistency-interface";
+import { Order } from "./order";
+import { Products } from "./products";
 import { ShoppingCart } from "./shopping-cart";
 
 const shoppingCart = new ShoppingCart();
-shoppingCart.AddItems({
-  name: 'Abobora',
-  price: 3.0,
-});
-shoppingCart.AddItems({
-  name: 'pimentao',
-  price: 4.0,
-});
-shoppingCart.AddItems({
-  name: 'alface',
-  price: 8.0,
-});
+const message = new Messaging();
+const persistency = new Persisting();
+const order = new Order(shoppingCart, message, persistency);
+
+shoppingCart.AddItems(new Products('Camiseta', 50.0));
+shoppingCart.AddItems(new Products('alface', 4.0));
+shoppingCart.AddItems(new Products('pimentao', 8.0));
 
 console.log(shoppingCart.items);
 console.log(shoppingCart.total());
-shoppingCart.checkOut();
-console.log(shoppingCart.orderStatus);
+console.log(order.orderStatus);
+order.checkOut();
+console.log(order.orderStatus);
