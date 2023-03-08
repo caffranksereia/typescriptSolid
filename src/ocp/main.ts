ae99b5ef88a006c59faa7c7324e89113a6a0035d
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-08 16:22:43 -0300

    mudei o nome do src para ocp

---
+++ src/ocp/main.ts
@@ -0,0 +1,20 @@
+import { Messaging } from "./services/message-interface";
+import { Persisting } from "./services/persistency-interface";
+import { Order } from "./entities/order";
+import { Products } from "./products";
+import { ShoppingCart } from "./shopping-cart";
+
+const shoppingCart = new ShoppingCart();
+const message = new Messaging();
+const persistency = new Persisting();
+const order = new Order(shoppingCart, message, persistency);
+
+shoppingCart.AddItems(new Products('Camiseta', 50.0));
+shoppingCart.AddItems(new Products('alface', 4.0));
+shoppingCart.AddItems(new Products('pimentao', 8.0));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(order.orderStatus);
+order.checkOut();
+console.log(order.orderStatus);
