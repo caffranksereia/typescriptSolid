669b62eedd00012da9c72fbde17bfa890601db26
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-09 17:26:57 -0300

    vantagens e desvantaggem

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-Liskov Substition Princeple
+/**
+Vantagens e desvantagem de usar S.O.L.I.D.
+
+Vantagens:
+- Codigo modular
+- codigo reutilizava ( D.R.Y - Dont repeat yourself)
+- codigo testavel, baixo acomplamento
+- Baixo acoplamento e alta coesa
+= codigo expasivel
+- Facil manutencao
+- Separations of concerns (Separação de conceitos)
+
+
+Desvantagens:
+- Complexidade
+- Quantidade de código digitado aumenta
+- Tempo de desenvolvimento aumenta bastante
+- Tenha cuidados com: YAGNI, KISS (You aren't gonna need it, Keep it simple, stupid!)
+
+ */
